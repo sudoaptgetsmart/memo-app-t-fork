4ec2fe3acfff7ef23b0cc74f68b436b121206ab3
adam-street <adam.street.dev@gmail.com>
2022-05-27 14:23:17 -0500

    added test for pending and finished list rendering

--- src/components/memo-list/MemoList.test.js
+++ src/components/memo-list/MemoList.test.js
@@ -32,3 +32,28 @@ test('should pass correct Memo prop and onEditMemo prop to each memo comp', () =
     expect(memoProp).toBe(_list[0])
     expect(onEditSelectProp).toBe(_onEditSelect)
 })
+
+test('should split finished memos and pending memos into two lists', () => {
+    const list = [
+        {id: 1, finished: true},
+        {id: 2, finished: false},
+        {id: 3, finished: true},
+        {id: 4, finished: false}
+    ]
+
+    let finishedList = []
+    let pendingList = []
+    const _Memo = ({memo}) => {
+        if (memo.finished) {
+            finishedList.push(memo.id)
+        } else {
+            pendingList.push(memo.id)
+        }
+        return <div>MOCK</div>
+    }
+
+    render(<MemoList list={list} _Memo={_Memo}/>)
+
+    expect(finishedList).toStrictEqual([1,3])
+    expect(pendingList).toStrictEqual([2,4])
+})
